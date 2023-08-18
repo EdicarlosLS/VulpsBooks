544ef72b848fbecd43467714009ddd7fa1b76106
EdicarlosLS <jels.23@hotmail.com>
2023-08-18 19:21:28 -0300

    Modal dinamico de livro - fake

--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@ let listaLivros = document.getElementById("lista-livros");
 let livros = [];
 
 for (let i = 0; i < 10; i++) {
-	let livro = {titulo : "Titulo " + i, capa : "./img/capa.png"};
+	let livro = {
+		titulo : "Titulo " + i,
+		capa : "./img/capa.png",
+		autores: [
+			"Autor " + i,
+			"Outro Autor " + i
+		],
+		publicacao: "10-10-2010",
+		editora: "Editora " + i,
+		descricao: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.",
+		categorias: [
+			"Ação " + i,
+			"Aventura " + i
+		]
+	};
 	livros.push(livro);
 }
 
@@ -24,6 +38,7 @@ for (let index = 0; index <= listaLivros.childNodes.length; index++) {
 	if(child){
 		child.addEventListener("click",()=>{
 			modal.style.display = "block";
+			carregarModalComLivro(index);
 		});
 	}
 }
@@ -32,3 +47,41 @@ let btnClose = document.getElementById("close");
 btnClose.addEventListener("click", ()=>{
 	modal.style.display= "none";
 });
+
+function carregarModalComLivro(index) {
+	let titulo = document.getElementById("titulo");
+	let capa = document.getElementById("capa");
+	let autores = document.getElementById("autores");
+	let publicacao = document.getElementById("publicacao");
+	let editora = document.getElementById("editora");
+	let descricao = document.getElementById("descricao");
+	let categorias = document.getElementById("categorias");
+
+	let livro = livros[index];
+
+	titulo.innerText = livro.titulo;
+	capa.setAttribute("src", livro.capa);
+	autores.innerHTML = "";
+	livro.autores.forEach((autor)=> autores.appendChild(spanFromAutor(autor)));
+	publicacao.innerText = livro.publicacao;
+	editora.innerText = livro.editora;
+	descricao.innerText = livro.descricao;
+	titulo.innerText = livro.titulo;
+	categorias.innerHTML = "";
+	livro.categorias.forEach((categoria)=> categorias.appendChild(spanFromCategoria(categoria)));
+
+}
+
+function spanFromAutor(autor){
+	let span = document.createElement("span");
+	span.classList.add("autor");
+	span.innerText = autor;
+	return span;
+}
+
+function spanFromCategoria(categoria){
+	let span = document.createElement("span");
+	span.classList.add("categoria");
+	span.innerText = categoria;
+	return span;
+}
